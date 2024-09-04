807444a9db3b44dd7d1a36bf2ba302a634865f78
Isabellalp21 <isabellalopespacheco6@gmail.com>
2024-09-04 02:36:38 -0300

    first commit

---
+++ Calcualdorateste.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+
+namespace projetoxunit
+
+{
+    public class Calcualdorateste
+    {
+        [Fact]
+        public void TesteSomar()
+        {
+            double resultado = Calculadora.Somar(10, 3);
+            Assert.Equal(13, resultado);
+        }
+        [Fact]
+        public void TesteSubtrair()
+        {
+            double resultado = Calculadora.Subtrair(10, 8);
+            Assert.Equal(2, resultado);
+        }
+        [Fact]
+        public void TesteMultiplicar()
+        {
+            double resultado = Calculadora.Multiplicar(2, 2);
+            Assert.Equal(4, resultado);
+        }
+        [Fact]
+        public void TesteDividir()
+        {
+            double resultado = Calculadora.Dividir(10, 2);
+            Assert.Equal(5, resultado);
+        }
+        [Fact]
+        public void TesteDividirPorZero()
+        {
+            double resultado = Calculadora.DividirPorZero(10,0);
+            Assert.Equal(double.PositiveInfinity, resultado);
+        }
+
+    }
+}
---
+++ Calculadora.cs
@@ -0,0 +1,27 @@
+﻿
+namespace projetoxunit
+{
+    public static class Calculadora
+    {
+        public static double Somar(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Subtrair(double x, double y)
+        {
+            return x - y;
+        }
+        public static double Multiplicar(double x, double y)
+        {
+            return x * y;
+        }
+        public static double Dividir(double x, double y)
+        {
+            return x / y;
+        }
+        public static double DividirPorZero(double numerador, double denominador)
+        {
+            return numerador / denominador;
+        }
+    }
+}
